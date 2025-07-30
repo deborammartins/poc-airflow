c10714cf95e159e8bb66a46fb90abd5aac43e816
deborammartins <deboramarquesmartins@gmail.com>
2025-07-30 12:38:27 -0300

    Update connection configurations

--- dags/scripts/dag_pubsub_bq/task_create_table.py
+++ dags/scripts/dag_pubsub_bq/task_create_table.py
@@ -1,3 +1,4 @@
+from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
 from google.cloud import bigquery
 
 def create_table(table_id: str, schema: dict):
@@ -5,7 +6,8 @@ def create_table(table_id: str, schema: dict):
     Cria uma tabela no BigQuery com o esquema fornecido.
     """
 
-    client = bigquery.Client()
-    table = bigquery.Table(table_id, schema=schema)
-    table = client.create_table(table, exists_ok=True)  # evita erro se já existir
+    hook = BigQueryHook(gcp_conn_id="connection_bq")
+    client = hook.get_client()
+    table = bigquery.Table(table_id, schema=[bigquery.SchemaField(**field) for field in schema])
+    client.create_table(table, exists_ok=True)
     print(f"Tabela criada: {table.full_table_id}")
